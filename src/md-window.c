e865d5f876ca3577b4490e8ed47725760aca1b74
Jonathan Kang <jonathankang@gnome.org>
2018-12-07 16:13:22 +0800

    Add md-window.ui
    Use .ui file to draw the interface for future expansion.

--- src/md-window.c
+++ src/md-window.c
@@ -29,11 +29,17 @@ G_DEFINE_TYPE (MdWindow, md_window, GTK_TYPE_APPLICATION_WINDOW)
 static void
 md_window_init (MdWindow *window)
 {
+    gtk_widget_init_template (GTK_WIDGET (window));
 }
 
 static void
 md_window_class_init (MdWindowClass *klass)
 {
+    GtkWidgetClass *widget_class;
+
+    widget_class = GTK_WIDGET_CLASS (klass);
+    gtk_widget_class_set_template_from_resource (widget_class,
+                                                 "/com/jonathankang/MigraineDiary/md-window.ui");
 }
 
 GtkWidget *
